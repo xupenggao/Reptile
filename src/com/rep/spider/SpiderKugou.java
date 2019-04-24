7ab9d8c7a687771caeb89097e73fa9ef774d2f4d
xupenggao <xupg@e-touch.cn>
2019-04-24 22:27:39 +0800

    项目初建

---
+++ src/com/rep/spider/SpiderKugou.java
@@ -0,0 +1,19 @@
+package com.rep.spider;
+
+import net.sf.json.JSONObject;
+
+/**
+ * @Author xupenggao
+ * @Date 22:14 2019/4/24
+ * @Description:
+ **/
+public class SpiderKugou {
+
+    public static String filePath = "";
+    public static String mp3 = "";
+    public static String link = "";
+
+    public static void main(String[] args) {
+        System.out.println("Hello World------");
+    }
+}
